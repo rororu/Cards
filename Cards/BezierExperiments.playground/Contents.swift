3d5de1743681c688f12b01f167572db83a612c5a
rororu <xylose.own@gmail.com>
2022-12-22 13:29:58 +0300

    Add project

---
+++ Cards/BezierExperiments.playground/Contents.swift
@@ -0,0 +1,35 @@
+//: A UIKit based Playground for presenting user interface
+  
+import UIKit
+import PlaygroundSupport
+
+class MyViewController : UIViewController {
+    override func loadView() {
+        let view = UIView()
+        view.backgroundColor = .white
+        self.view = view
+        
+        createBezier(on: view)
+    }
+    
+    private func createBezier(on view: UIView) {
+        let shapeLayer = CAShapeLayer()
+        
+        view.layer.addSublayer(shapeLayer)
+        
+        shapeLayer.strokeColor = UIColor.gray.cgColor
+        shapeLayer.lineWidth = 5
+        
+        shapeLayer.path = getPath().cgPath
+    }
+    
+    private func getPath() -> UIBezierPath {
+        let path = UIBezierPath()
+        path.move(to: CGPoint(x: 50, y: 50))
+        path.addLine(to: CGPoint(x: 150, y: 50))
+        
+        return path
+    }
+}
+// Present the view controller in the Live View window
+PlaygroundPage.current.liveView = MyViewController()
