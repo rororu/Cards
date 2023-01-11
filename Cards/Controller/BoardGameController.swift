a53e6a5ba5b4b3cdbfbc11ae973fc551dfed8d1a
rororu <xylose.own@gmail.com>
2023-01-12 01:19:30 +0300

    Add boarderView on viewRoot

--- Cards/Controller/BoardGameController.swift
+++ Cards/Controller/BoardGameController.swift
@@ -11,6 +11,15 @@ class BoardGameController: UIViewController {
     
     var cardsPairsCounts = 8
     lazy var game: Game = getNewGame()
+    lazy var startButtonView = getStartButtonView()
+    lazy var boardGameControll = getBoardGameControll()
+
+    override func loadView() {
+        super.loadView()
+        
+        view.addSubview(startButtonView)
+        view.addSubview(boardGameControll)
+    }
     
     private func getNewGame() -> Game {
         let game = Game()
@@ -19,11 +28,46 @@ class BoardGameController: UIViewController {
         
         return game
     }
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
+    private func getStartButtonView() -> UIButton {
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 200, height: 50))
+        
+        let window = UIApplication.shared.windows[0]
+        let topPadding = window.safeAreaInsets.top
+        
+        button.center.x = view.center.x
+        button.frame.origin.y = topPadding
+        
+        button.setTitle("Начать игру", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.setTitleColor(.gray, for: .highlighted)
+        button.backgroundColor = .systemGray4
+        button.layer.cornerRadius = 10
+        button.addTarget(nil, action: #selector(startGame(_:)), for: .touchUpInside)
+        
+        return button
+    }
+    
+    private func getBoardGameControll() -> UIView {
+        let margin: CGFloat = 10
+        let boardView = UIView()
+        let window = UIApplication.shared.windows[0]
+        let topPadding = window.safeAreaInsets.top
+        let bottomPadding = window.safeAreaInsets.bottom
+        
+        boardView.frame.origin.x = margin
+        boardView.frame.origin.y = topPadding + startButtonView.frame.height + margin
+        boardView.frame.size.width = UIScreen.main.bounds.width - margin * 2
+        boardView.frame.size.height = UIScreen.main.bounds.height - boardView.frame.origin.y - margin - bottomPadding
+        
+        boardView.layer.cornerRadius = 5
+        boardView.backgroundColor = UIColor(red: 0.1, green: 0.9, blue: 0.1, alpha: 0.3)
+        
+        return boardView
+    }
+    
+    @objc func startGame(_ sender: UIButton) {
+        print("button was pressed")
     }
 
 }
